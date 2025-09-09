5c6ad622f3880050eea1bdd358dc5a5a7b2ffe6d
Anmol Agarwal <anmolagarwal325@gmail.com>
2025-09-09 17:01:24 +0530

    // vite.config.js import { defineConfig } from 'vite' import react from '@vitejs/plugin-react'
    // If your repo name is "my-intro-site", keep this base:
    export default defineConfig({
      plugins: [react()],
      base: '/my-intro-site/',
    })

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// If your repo name is "my-intro-site", keep this base:
 export default defineConfig({
   plugins: [react()],
   base: '/my-intro-site/',
